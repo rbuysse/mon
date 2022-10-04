eb98cf3705698da83d2a4dc0aadc5f3be6813c3f
Ryan Beck-Buysse <rbuysse@gmail.com>
2022-10-04 11:50:09 -0500

    Add toml config file for site definitions
    Signed-off-by: Ryan Beck-Buysse <rbuysse@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rbuysse/mon
 
 go 1.19
+
+require github.com/BurntSushi/toml v1.2.0 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
+
+	"github.com/BurntSushi/toml"
 )
+
+type Config struct {
+	Sites []*Site `toml:"site"`
+}
+
+type Site struct {
+	Url     string `toml:"url"`
+	Pattern string `toml:"pattern"`
+}
 
 func checkForPattern(body string, pattern string) (check bool) {
 	if strings.Contains(body, pattern) {
@@ -15,6 +28,20 @@ func checkForPattern(body string, pattern string) (check bool) {
 		check = false
 	}
 	return
+}
+
+func getConfig(f string) (c Config) {
+	if _, err := os.Stat(f); err != nil {
+		log.Fatal(err)
+	}
+
+	_, err := toml.DecodeFile(f, &c)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	return c
 }
 
 func getPage(url string) (page string, err error) {
@@ -37,23 +64,26 @@ func getPage(url string) (page string, err error) {
 }
 
 func main() {
+	configFile := "config.toml"
 	for {
-		urlToCheck := "https://example.com"
-		pattern := "<title>Exampl Domain"
+		c := getConfig(configFile)
+		for i := 0; i < len(c.Sites); i++ {
+			fmt.Println(c.Sites[i].Url)
+			urlToCheck := c.Sites[i].Url
+			pattern := c.Sites[i].Pattern
 
-		resp, status := getPage(urlToCheck)
+			resp, status := getPage(urlToCheck)
 
-		if status != nil {
-			fmt.Printf("ERROR: %s - %s\n", urlToCheck, status)
-		} else {
-			patternExists := checkForPattern(resp, pattern)
-			if patternExists == true {
+			if status != nil {
+				fmt.Printf("ERROR: %s - %s\n", urlToCheck, status)
 			} else {
-				fmt.Printf("ERROR: %s - Pattern not found: %s\n", urlToCheck, pattern)
+				patternExists := checkForPattern(resp, pattern)
+				if patternExists == true {
+				} else {
+					fmt.Printf("ERROR: %s - Pattern not found: %s\n", urlToCheck, pattern)
+				}
 			}
-
 		}
-
 		time.Sleep(5 * time.Second)
 	}
 }
