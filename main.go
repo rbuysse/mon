21b1eac1b1d860b9d2112809cc6613172f24db5c
Ryan Beck-Buysse <rbuysse@gmail.com>
2022-10-22 14:27:30 -0500

    Write metrics to influx
    Signed-off-by: Ryan Beck-Buysse <rbuysse@gmail.com>

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -35,6 +37,24 @@ func checkForPattern(body string, pattern string) (check bool) {
 		check = false
 	}
 	return
+}
+
+func dbConnect() (influxdb2.Client, error) {
+
+	token := os.Getenv("INFLUXDB_TOKEN")
+	if token == "" {
+		return nil, errors.New("INFLUXDB_TOKEN must be set")
+	}
+
+	url := os.Getenv("INFLUXDB_URL")
+	if url == "" {
+		return nil, errors.New("INFLUXDB_URL must be set")
+	}
+
+	client := influxdb2.NewClient(url, token)
+	_, err := client.Health(context.Background())
+
+	return client, err
 }
 
 func getConfig(f string) (c Config) {
@@ -90,6 +110,19 @@ func main() {
 	configFile := "config.toml"
 	for {
 		c := getConfig(configFile)
+
+		const bucket = "mon"
+		const org = "mon"
+
+		client, err := dbConnect()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+			os.Exit(1)
+		}
+
+		writeAPI := client.WriteAPIBlocking("mon", "mon")
+
 		loopstart := time.Now()
 		ch := make(chan Result)
 
@@ -101,7 +134,17 @@ func main() {
 		}
 
 		for i := 0; i < len(c.Sites); i++ {
+			var dbErr string
 			ret := <-ch
+			if ret.Error != nil {
+				dbErr = fmt.Sprintf("%v", ret.Error)
+			}
+			p := influxdb2.NewPointWithMeasurement("stat").
+				AddTag("host", ret.Url).
+				AddField("resp", ret.Duration).
+				AddField("error", dbErr).
+				SetTime(time.Now())
+			writeAPI.WritePoint(context.Background(), p)
 			fmt.Printf("%s - %v - %.2fs\n", ret.Url, ret.Error, ret.Duration)
 		}
 		fmt.Printf("%.2fs elapsed\n\n", time.Since(loopstart).Seconds())
